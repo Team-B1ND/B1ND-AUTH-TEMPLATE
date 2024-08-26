0cc7ffc9a0b5994e7e30aeeaac04070133997a9a
wjzlskxk <chan2bo2@dgsw.hs.kr>
2024-08-26 20:54:12 +0900

    fix :: 공백제거 열맞춤

--- axios/customAxiosGenerator.js
+++ axios/customAxiosGenerator.js
@@ -10,30 +10,30 @@ function createCustomAxiosFile(projectName) {
   }
 
   const customAxiosCode = `
-  import CONFIG from "src/config/config.json";
-  import axios, { AxiosRequestConfig, AxiosError } from "axios";
-  import requestInterceptor from "./requestInterceptor";
-  import ResponseHandler from "./responseInterceptor";
-  import Token from "../token/token";
-  import { REQUEST_TOKEN_KEY, ACCESS_TOKEN_KEY } from "src/constants/token/token.constants";
-  const axiosRequestConfig: AxiosRequestConfig = {
-    baseURL: CONFIG.server,
-    headers: {
-      [REQUEST_TOKEN_KEY]: \`Bearer \${Token.getToken(ACCESS_TOKEN_KEY)}\`,
-    },
-  };
-  
-  const customAxios = axios.create(axiosRequestConfig);
-  
-  customAxios.interceptors.request.use(requestInterceptor as any, (err: AxiosError) => Promise.reject(err));
-  
-  customAxios.interceptors.response.use((res) => res, ResponseHandler);
-  
-  export default customAxios;
-  
-  export const setAccessToken = (token: string) => {
-    customAxios.defaults.headers[REQUEST_TOKEN_KEY] = \`Bearer \${token}\`;
-  };
+import CONFIG from "src/config/config.json";
+import axios, { AxiosRequestConfig, AxiosError } from "axios";
+import requestInterceptor from "./requestInterceptor";
+import ResponseHandler from "./responseInterceptor";
+import Token from "../token/token";
+import { REQUEST_TOKEN_KEY, ACCESS_TOKEN_KEY } from "src/constants/token/token.constants";
+const axiosRequestConfig: AxiosRequestConfig = {
+  baseURL: CONFIG.server,
+  headers: {
+    [REQUEST_TOKEN_KEY]: \`Bearer \${Token.getToken(ACCESS_TOKEN_KEY)}\`,
+  },
+};
+
+const customAxios = axios.create(axiosRequestConfig);
+
+customAxios.interceptors.request.use(requestInterceptor as any, (err: AxiosError) => Promise.reject(err));
+
+customAxios.interceptors.response.use((res) => res, ResponseHandler);
+
+export default customAxios;
+
+export const setAccessToken = (token: string) => {
+  customAxios.defaults.headers[REQUEST_TOKEN_KEY] = \`Bearer \${token}\`;
+};
 `;
 
   // token.ts 파일 생성
