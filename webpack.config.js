be3b431fea3804775de4a6d6ad5bf28dddb8ff20
해준 <127281057+ftery0@users.noreply.github.com>
2024-09-03 14:08:51 +0900

    feat :: webpack

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const svgToMiniDataURI = require("mini-svg-data-uri");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx"],
+        fallback: {
+            abc: false, 
+            crypto: require.resolve("crypto-browserify"), 
+        },
+        alias: {
+            "@src": path.resolve(__dirname, "./src"),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts|js|jsx)$/,
+                loader: "esbuild-loader",
+                options: {
+                    loader: "tsx",
+                    target: "es2016",
+                },
+            },
+            {
+                test: /\.(png|jpg|gif|mp4|jpeg)$/,
+                type: "asset",
+            },
+            {
+                test: /\.svg/,
+                type: "asset",
+                generator: {
+                    dataUrl: (content) => {
+                        content = content.toString();
+                        return svgToMiniDataURI(content);
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "esbuild-loader",
+                        options: {
+                            loader: "css",
+                            minify: true,
+                        },
+                    }
+                ],
+            },
+        ],
+    },
+    output: { 
+        path: path.resolve(__dirname, "build"),
+        clean: true,
+        publicPath: "/",
+        filename: "[name].[chunkhash].js",
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "public", "index.html"),
+            hash: true,
+            favicon: path.resolve(__dirname, "public", "favicon.ico"), 
+        }),
+ 
+    ] 
+};
